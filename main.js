6085555ed6cc72a57714f35a8366bba1b0420021
Muhammad Muttraf Rasool <muttrafgujjar.787@gmail.com>
2022-11-30 18:52:21 +0500

    Added add and remove book function in class

--- main.js
+++ main.js
@@ -1,95 +1,54 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
 const showBooks = document.querySelector('.record');
 const Title = document.querySelector('.title');
 const Author = document.querySelector('.author');
 const btn = document.querySelector('.form button');
 
-let books = [];
-
 class Book {
-  constructor(Title, Author) {
-    this.title = Title;
-    this.author = Author;
+  constructor() {
+    this.books = [];
   }
 
-  addElement() {
-    showBooks.innerHTML = books
-      .map(
-        (i) => `
-      <div>
-      <h5>${i?.title} by ${i?.author} </h5>
-      <button onClick='removeBook(${JSON.stringify(i?.title)})'>Remove</button>
-      </div>`
-      )
-      .join('');
+  addBook(obj) {
+    this.books = JSON.parse(localStorage.getItem('data'));
+    this.books.push(obj);
+    localStorage.setItem('data', JSON.stringify(this.books));
+    window.location.reload();
   }
 
-  removeElement() {}
+  removeBook(title) {
+    this.books = JSON.parse(localStorage.getItem('data'));
+    this.books = this.books.filter((i) => i?.title !== title);
+    localStorage.setItem('data', JSON.stringify(this.books));
+    window.location.reload();
+  }
 }
 
+const bookc = new Book();
+
 btn.addEventListener('click', () => {
-  books = JSON.parse(localStorage.getItem('data'));
-
-  const bookc = new Book(Title.value, Author.value);
-  console.log(bookc);
-
-  // const book = {
-  //   title: Title.value,
-  //   author: Author.value,
-  // };
-
-  // books = JSON.parse(localStorage.getItem('data'));
-  // console.log(book);
-
-  books.push(bookc);
-  localStorage.setItem('data', JSON.stringify(books));
-
-  bookc.addElement();
-
-  // showBooks.innerHTML = books
-  //   .map(
-  //     (i) => `
-  //     <div>
-  //     <h5>${i?.title} by ${i?.author} </h5>
-  //     <button onClick='removeBook(${JSON.stringify(i?.title)})'>Remove</button>
-  //     </div>`
-  //   )
-  //   .join('');
+  const bookObj = {
+    title: Title.value,
+    author: Author.value,
+  };
+  bookc.addBook(bookObj);
 });
-
-const removeBook = (title) => {
-  books = JSON.parse(localStorage.getItem('data'));
-  books = books.filter((i) => i?.title !== title);
-  localStorage.setItem('data', JSON.stringify(books));
-  showBooks.innerHTML = books
-    .map(
-      (i) => `
-      <div>
-      <h5>${i?.title} by ${i?.author} </h5>
-      <button class="test" onClick='removeBook(${JSON.stringify(
-        i?.title
-      )})'>Remove</button>
-      </div>`
-    )
-    .join('');
-  // window.location.reload();
-};
 
 // ***********
 
 // Locale storage
 
 window.addEventListener('DOMContentLoaded', () => {
-  // localStorage.setItem('data', JSON.stringify(books));
-  books = JSON.parse(localStorage.getItem('data'));
+  const books = JSON.parse(localStorage.getItem('data'));
   showBooks.innerHTML = books
     .map(
       (i) => `
       <div class="element">
       <h5>${i?.title} by ${i?.author} </h5>
-      <button onClick='removeBook(${JSON.stringify(i?.title)})'>Remove</button>
+      <button onClick='bookc.removeBook(${JSON.stringify(i?.title)})'>Remove</button>
       </div>
-      `
+      `,
     )
     .join('');
 });
